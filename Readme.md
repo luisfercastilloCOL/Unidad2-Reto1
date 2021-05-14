b723ee46d314eebdd7fd3f08f746f17c55317058
LuisFernando187 <>
2021-05-14 20:48:34 +0000

    Initial commit

---
+++ Readme.md
@@ -0,0 +1,14 @@
+# RETO SEMANA 2
+### Puerta del Castillo
+El rey Arturito (antecesor de R2D2), está muy preocupado porque le han informado que el ejército Vaderiano está muy cerca de su castillo con la idea de atacarlo.
+ El castillo se
+encuentra realmente muy bien protegido por murallas y cañones, pero su talón de Aquiles
+es la gran puerta del castillo, que solo se puede cerrar bajo ataque, porque por allí entran
+víveres, productos y medicinas.
+El problema es que se demoran mucho para cerrarla ya que esto se hace empujándola de
+abajo hacia arriba por los soldados Chewbaccas, que a pesar de su fuerza, en ocasiones no
+alcanzan a cerrarla completamente y el castillo es saqueado.
+A oídos del rey ha llegado un plano elaborado por un tal Arquímedes (se sospecha que es
+un seudónimo) donde le plantea una solución al problema aunque no da muchos detalles.
+Aquí reproducimos el dibujo y las instrucciones
+![CastilloArturito](CastilloArturito.png)
---
+++ main.py
@@ -0,0 +1,78 @@
+### Implementar las funciones del ejercicio de la puerta
+""" REto 2 Puerta del Castillo #
+    Luis Fernando Castillo Ossa
+    Mayo 13-2021 """
+# Definición de Funciones
+# Pruebas automáticas
+
+def cantidad_vueltas_cierra(largom, dpoleacm):
+  """ Parameter
+      --------
+      largom
+        type:int 
+        description:operando largo puerta del castillo en metros
+      
+      rpoleacm:
+      type:int
+      description:Diametro de la polea en centímetros 
+
+      Returns:
+      ----------
+      cantidad_vueltas
+      type:int
+      description:  Cantidad de vueltas que debe dar la polea  para abrir la 
+       Parameters
+      ---------- 
+  """ 
+
+
+def cantidad_chewebacas(largom, dpoleacm):
+  """ Parameter
+      --------
+      largom
+        type:int 
+        description:operando largo puerta del castillo en metros
+      
+      rpoleacm:
+      type:int
+      description:Diametro de la polea en centímetros 
+
+      Returns:
+      ----------
+      cantidad_chewebacas
+      type:int
+      description:  Cantidad de chewebacas  para abrir la puerta
+       Parameters
+      ---------- 
+      Suggestions:
+      Trate de usar la función que determina la cantidad de vueltas Para abrir la puerta, sólo un chewebacas puede dar 3 giros (El valor debe ser la cantidad entera )
+  """ 
+
+def velocidad_cerrar_puerta_min(largom, rpoleacm, minutoss ):
+  """ Parameter
+      --------
+      largom
+        type:int 
+        description:operando largo puerta del castillo en metros
+      
+      rpoleacm:
+      type:int
+      description:Radio de la polea en centímetros 
+
+      minutos:
+      type:int
+      description:cantidad limite minutos para cerrar la puerta, 
+
+      Returns:
+      ----------
+      velocidad_cierra
+      type:int
+      description:  velocidad en cm/seg para girar la polea 
+       Parameters
+      ---------- 
+      Suggestions:
+      Tener en cuenta la fórmula de Velocidad=espacio/tiempo
+  """ 
+
+
+
